74ec1148fe7deed583f736379bb58c659a566363
Sarin CR <sarinrays@gmail.com>
2022-11-18 14:50:37 +0530

    LR

--- 05.Log.py
+++ 05.Log.py
@@ -1,16 +1,16 @@
 import math
 import numpy as np
 
-X = [1000, 20000, 30000]
-Y = [11, 4, 10]
+X = [.1, -.010, .010]
+Y = [.11, .1, .10]
 
-loss = (X[0] * Y[0] +   X[1] * Y[1] +    X[2] * Y[2])
-
-z=math.log(loss)
-
-print(z,loss)
+K = (X[0] * Y[0] +   X[1] * Y[1] +    X[2] * Y[2])
 
 
-a=1/(1 + np.exp(-loss))
+
+print(K)
+
+
+a=1/(1 + np.exp(-K))
 
 print(a)
